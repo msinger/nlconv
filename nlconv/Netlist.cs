3a45ddb9d91c75ea94ae1f75a0a96b94cf188610
Michael Singer <michael@a-singer.de>
2022-10-18 22:52:27 +0200

    Change Java Script generator

--- nlconv/Netlist.cs
+++ nlconv/Netlist.cs
@@ -779,88 +779,103 @@ namespace nlconv
 
 		public virtual void ToJavaScript(TextWriter s)
 		{
-			var c = new Dictionary<string, Dictionary<string, (string, string, List<float>)>>();
+			var c = new Dictionary<string, List<string>>();
+			s.WriteLine("var cells_cn={");
 			foreach (var x in Cells)
 			{
 				if (!x.Value.Coords.ContainsKey(""))
 					continue;
 				var n = x.Value.Name;
-				var wb = n.WithoutBars();
+				var wb = n.WithoutBars().ToLowerInvariant();
+				s.Write("\"");
+				s.Write(n.ToJavaScriptString());
+				s.Write("\":{h:\"");
+				s.Write(n.ToUpperInvariant().ToHtml().ToJavaScriptString());
+				s.Write("\",a:\"c_");
+				s.Write(n.ToHtmlId().ToJavaScriptString());
+				s.Write("\",l:[");
+				s.Write(x.Value.Coords[""][0][0].ToString(CultureInfo.InvariantCulture));
+				s.Write(",");
+				s.Write(x.Value.Coords[""][0][1].ToString(CultureInfo.InvariantCulture));
+				s.Write(",");
+				s.Write(x.Value.Coords[""][0][2].ToString(CultureInfo.InvariantCulture));
+				s.Write(",");
+				s.Write(x.Value.Coords[""][0][3].ToString(CultureInfo.InvariantCulture));
+				s.WriteLine("]},");
 				if (!c.ContainsKey(wb))
-					c[wb] = new Dictionary<string, (string, string, List<float>)>();
-				c[wb][n] = (n.ToHtml(), n.ToHtmlId(), x.Value.Coords[""][0]);
+					c[wb] = new List<string>();
+				c[wb].Add(n);
+				foreach (var y in x.Value.Alias)
+				{
+					wb = y.WithoutBars().ToLowerInvariant();
+					if (!c.ContainsKey(wb))
+						c[wb] = new List<string>();
+					c[wb].Add(n);
+				}
 			}
+			s.WriteLine("};");
 
-			var w = new Dictionary<string, Dictionary<string, (string, string, List<List<float>>)>>();
+			var w = new Dictionary<string, List<string>>();
+			s.WriteLine("var wires_cn={");
 			foreach (var x in Wires)
 			{
 				if (x.Value.Coords.Count == 0)
 					continue;
 				var n = x.Value.Name;
-				var wb = n.WithoutBars();
-				if (!w.ContainsKey(wb))
-					w[wb] = new Dictionary<string, (string, string, List<List<float>>)>();
-				w[wb][n] = (n.ToHtml(), n.ToHtmlId(), x.Value.Coords);
-			}
-
-			s.WriteLine("var cells={");
-			foreach (var i in c)
-			{
+				var wb = n.WithoutBars().ToLowerInvariant();
 				s.Write("\"");
-				s.Write(i.Key);
-				s.Write("\":{");
-				foreach (var j in i.Value)
+				s.Write(n.ToJavaScriptString());
+				s.Write("\":{h:\"");
+				s.Write(n.ToUpperInvariant().ToHtml().ToJavaScriptString());
+				s.Write("\",a:\"c_");
+				s.Write(n.ToHtmlId().ToJavaScriptString());
+				s.Write("\",l:[");
+				foreach (var y in x.Value.Coords)
 				{
-					s.Write("\"");
-					s.Write(j.Key);
-					s.Write("\":{h:\"");
-					(string html, string anchor, List<float> l) tp = j.Value;
-					s.Write(tp.html.ToJavaScriptString());
-					s.Write("\",a:\"c_");
-					s.Write(tp.anchor.ToJavaScriptString());
-					s.Write("\",l:[");
-					s.Write(tp.l[0].ToString(CultureInfo.InvariantCulture));
-					s.Write(",");
-					s.Write(tp.l[1].ToString(CultureInfo.InvariantCulture));
-					s.Write(",");
-					s.Write(tp.l[2].ToString(CultureInfo.InvariantCulture));
-					s.Write(",");
-					s.Write(tp.l[3].ToString(CultureInfo.InvariantCulture));
-					s.Write("]},");
+					s.Write("[");
+					foreach (var z in y)
+					{
+						s.Write(z.ToString(CultureInfo.InvariantCulture));
+						s.Write(",");
+					}
+					s.Write("],");
 				}
-				s.WriteLine("},");
+				s.WriteLine("]},");
+				if (!w.ContainsKey(wb))
+					w[wb] = new List<string>();
+				w[wb].Add(n);
 			}
 			s.WriteLine("};");
 
-			s.WriteLine("var wires={");
-			foreach (var i in w)
+			s.WriteLine("var cells_grp={");
+			foreach (var x in c)
 			{
 				s.Write("\"");
-				s.Write(i.Key);
-				s.Write("\":{");
-				foreach (var j in i.Value)
+				s.Write(x.Key);
+				s.Write("\":[");
+				foreach (var y in x.Value)
 				{
 					s.Write("\"");
-					s.Write(j.Key);
-					s.Write("\":{h:\"");
-					(string html, string anchor, List<List<float>> l) tp = j.Value;
-					s.Write(tp.html.ToJavaScriptString());
-					s.Write("\",a:\"w_");
-					s.Write(tp.anchor.ToJavaScriptString());
-					s.Write("\",l:[");
-					foreach (var k in tp.l)
-					{
-						s.Write("[");
-						foreach (var l in k)
-						{
-							s.Write(l.ToString(CultureInfo.InvariantCulture));
-							s.Write(",");
-						}
-						s.Write("],");
-					}
-					s.Write("]},");
+					s.Write(y.ToJavaScriptString());
+					s.Write("\",");
 				}
-				s.WriteLine("},");
+				s.WriteLine("],");
+			}
+			s.WriteLine("};");
+
+			s.WriteLine("var wires_grp={");
+			foreach (var x in w)
+			{
+				s.Write("\"");
+				s.Write(x.Key);
+				s.Write("\":[");
+				foreach (var y in x.Value)
+				{
+					s.Write("\"");
+					s.Write(y.ToJavaScriptString());
+					s.Write("\",");
+				}
+				s.WriteLine("],");
 			}
 			s.WriteLine("};");
 		}
